35493ab794f02814744b66fedfea86143b763b3d
Development for People <47140294+developmentforpeople@users.noreply.github.com>
2023-02-23 09:46:54 +0000

    UPDATE version increase and setup updates

--- setup.py
+++ setup.py
@@ -3,8 +3,7 @@ from setuptools import setup, find_packages
 with open("requirements.txt") as f:
 	install_requires = f.read().strip().split("\n")
 
-# get version from __version__ variable in delete/__init__.py
-from delete import __version__ as version
+from egd_site import __version__ as version
 
 setup(
 	name='egd_site',
